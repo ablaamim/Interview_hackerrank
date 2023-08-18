abb00c7fe598df1131b1c545971f7b1a0da11ce1
Abdessamad Laamimi <ablaamim@c4r9p6.1337.ma>
2023-08-18 21:42:48 +0100

    day00

---
+++ Day00/Plus_Minus/plusMinus.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void plusMinus(vector<int> arr)
+{
+    int i = 0;
+    int n_pos = 0;
+    int zeros = 0;
+    int n_neg = 0;
+    
+    while (i < arr.size())
+    {
+        if (arr[i] > 0)
+            n_pos++;
+        else if (arr[i] == 0)
+            zeros++;
+        else if (arr[i] < 0)
+            n_neg++;
+        i++;
+    }
+    std::cout << (float) n_pos / arr.size() << std::fixed << std::setprecision(6) << std::endl;
+    std::cout << (float) n_neg / arr.size() << std::fixed << std::setprecision(6) << std::endl;
+    std::cout << (float) zeros / arr.size() << std::fixed << std::setprecision(6) << std::endl;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
